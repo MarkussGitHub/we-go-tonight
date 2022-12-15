7f6e4ccc4df8220fec105a54313464312065aad1
Markuss <juhnevskis.markuss@gmail.com>
2022-12-16 00:06:44 +0200

    create db

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -17,6 +17,38 @@ class DBManager:
         )
         self.cursor = self.conn.cursor()
         self.conn.commit()
+        self.create_tables()
+    
+    def create_tables(self):
+        self.cursor.execute("""
+        BEGIN;
+
+
+        CREATE TABLE IF NOT EXISTS public.account
+        (
+            id uuid NOT NULL,
+            created timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            telegram_name character varying(36) COLLATE pg_catalog."default",
+            telegram_user_id character varying(256) COLLATE pg_catalog."default" NOT NULL,
+            refered_by uuid,
+            is_staff boolean NOT NULL DEFAULT false,
+            CONSTRAINT user_pkey PRIMARY KEY (id)
+        );
+
+        CREATE TABLE IF NOT EXISTS public.advert
+        (
+            id uuid NOT NULL,
+            advert_msg_id character varying(128) COLLATE pg_catalog."default" NOT NULL,
+            created_at time with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            send_time time with time zone DEFAULT CURRENT_TIMESTAMP,
+            owner_id uuid,
+            FOREIGN KEY (owner_id) REFERENCES account(id),
+            CONSTRAINT advert_pkey PRIMARY KEY (id)
+        );
+
+        END;
+        """)
+        self.conn.commit()
 
     def _convert_account_to_dict(self, raw):
         """Converts raw account from db to dict."""
@@ -27,7 +59,6 @@ class DBManager:
             "telegram_user_id": raw[3],
             "refered_by": raw[4],
             "is_staff": raw[5],
-            "is_moderator": raw[6],
         }
 
     def _convert_advert_to_dict(self, raw):
